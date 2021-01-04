524ae00d266c1e6887991131287fc731008c14d8
Matheus Seabra <matheus@academicmerit.com>
2021-01-04 19:24:24 -0300

    Client: Header component

---
+++ packages/client/src/components/Header/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export default styled.header`
+  height: 84px;
+  background-color: #8d67dd;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  .title {
+    color: #fff;
+    font-weight: 500;
+    font-size: 18px;
+  }
+`;
